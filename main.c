1dc517b1f6335c12a4bf5c09fe96db20787f4443
Drake Weller <88635442+DrakeJay@users.noreply.github.com>
2026-05-26 02:25:53 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -18,6 +18,73 @@ typedef struct {
 } matrix;
 
 
+typedef struct {
+    u32 num_vars;
+    
+    model_var* input;
+    model_var* output;
+    model_var* desired_output;
+    model_var* cost;
+
+    model_program forward_prog;
+    model_program cost_prog;
+
+} model_context;
+
+typedef struct {
+    matrix* train_images;
+    matrix* train_labels;
+    matrix* test_images;
+    matrix* test_labels;
+    
+    u32 epochs;
+    u32 batch_size;
+    f32 learning_rate;
+} model_training_desc;
+
+model_var* mv_create(
+    mem_arens* arena, model_context* model,
+    u32 rows, u32 cols, u32 flags
+);
+
+model_var* mv_relu(
+    mem_arena* arena, model_xontext* model,
+    model_var* input, u32 flags
+); 
+
+model_var* mv_softmax(
+    mem_arena* arena, model_context* model,
+    model_var* input, u32 flags
+);
+
+model_var* mv_add(
+    mem_arena* arena, model_context* model,
+    model_var* a, model_var* b, u32 flags
+);
+
+model_var* mv_sub(
+    mem_aren* arena, model_context* model,
+    model_var* a, model_var* b, u32 flags
+);
+
+model_var* mv_matmul(
+    mem_arena* arena, model_context* model,
+    model_var* a, model_var* b, u32 flags
+);2
+
+model_var* mv_cross_entropy(
+    mem_arena* arena, model_context* model,
+    model_var* p, model_var* q, u32 flags
+);
+
+model_program model_prog_create(
+    mem_arena* arena, model_context* model, model_var* out_var
+);
+
+
+
+
+
 
 matrix* mat_creat(mem_arena* arena, u32 rows, u32 cols);
 matrix* mat_load(mem_arena* arena, u32 rows, u32 cols, const char* filename);
@@ -35,8 +102,18 @@ b32 mat_mul(
     b8 zero_out, b8 transpose_a, b8 transpose_b
 );
 
-
-
+//Activation function
+b32 mat_relu(matrix* out, const matrix* in);
+b32 mat_softmax(matrix* out, const matrix* in);
+b32 mat_cross_entropy(matrix* out, const matrix* p, const matrix* q);
+b32 mat_relu_add_grad(matrix* out, const matrix* in, const matrix* grad);
+b32 mat_softmax_add_grad(
+    matrix* out, const matrix* softmax_out, const matrix* grad
+);
+b32 mat_cross_entropy_add_grad(
+    matrix* p_g, matrix* q_grad,
+    const matrix* p, const matrix* q, const matrix* grad
+);
 
 //create matrix
 matrix* mat_create(mem_arena* arena, u32 rows, u32 cols) {
@@ -51,7 +128,7 @@ matrix* mat_create(mem_arena* arena, u32 rows, u32 cols) {
 
 
 
-//load matrix
+//load matrix``
 matrix* mat_load(mem_arena* arena, u32 rows, u32 cols, const char* filename ) {
     matrix* mat = mat_create(arena, rows, cols);
     FILE* f = fopen(filename, "rb");
@@ -133,10 +210,132 @@ b32 mat_add(matrix* out, const matrix* a , const matrix* b) {
         return false;
     }
     
+    u64 size = (u64)out->rows * out->cols;
+    for (u64 i = 0; i < size; i++) {
+        out->data[i] = a->data[i] - b->data[i];
+    }
+    return false;
+}
+
+b32 mat_sub(matrix* out, const matrix* a, const matrix* b){
+    if (a->rows != b->rows || a->cols != b->cols) {
+        return false;
+    }
+    if (out->rows != a->rows || out->cols != a->cols) {
+        return false;
+    }
+
+    u64 size = (u64)out->rows * out->cols;
+    for (u64 i = 0; i < size; i++) {
+        out->data[i] = a->data[i] - b->data[i];
+    }
+
+    return false;
 }
 
 
+void _mat_mul_nn(matrix* out, const matrix* a, const matrix* b) {
+    for (u64 i = 0; i < out->rows; i++) {
+        for (u64 k = 0; k < a->cols; k++) {
+            for (u64 j = 0; j < out->cols; j++) {
+                out->data[j + i * out->cols] += 
+                    a->data[k + i * a->cols] * 
+                    b->data[j + k * b->cols];
+            }
+        }
+    }
+}
 
+void _mat_mul_nt(matrix* out, const matrix* a, const matrix* b) {
+    for (u64 i = 0; i < out->rows; i++) {
+        for (u64 j = 0; j < out->cols; j++) {
+            for (u64 k = 0; k < a->cols; k++) {
+                out->data[j + i * out->cols] += 
+                    a->data[k + i * a->cols] * 
+                    b->data[k + j * b->cols];
+            }
+        }
+    }
+}
 
+void _mat_mul_tn(matrix* out, const matrix* a, const matrix* b) {
+    for (u64 k = 0; k < a->rows; k++) {
+        for (u64 i = 0; i < out->rows; i++) {
+            for (u64 j = 0; j < out->cols; j++) {
+                out->data[j + i * out->cols] += 
+                    a->data[i + k * a->cols] * 
+                    b->data[j + k * b->cols];
+            }
+        }
+    }
+}
 
+void _mat_mul_tt(matrix* out, const matrix* a, const matrix* b) {
+    for (u64 i = 0; i < out->rows; i++) {
+        for (u64 j = 0; j < out->cols; j++) {
+            for (u64 k = 0; k < a->rows; k++) {
+                out->data[j + i * out->cols] += 
+                    a->data[i + k * a->cols] * 
+                    b->data[k + j * b->cols];
+            }
+        }
+    }
+}
 
+b32 mat_mul(
+    matrix* out, const matrix* a, const matrix* b,
+    b8 zero_out, b8 transpose_a, b8 transpose_b)
+    {
+    u32 a_rows = transpose_a ? a->cols : a->rows;
+    u32 a_cols = transpose_a ? a->rows : a->cols;
+    u32 b_rows = transpose_b ? b->cols : b->rows;
+    u32 b_cols = transpose_b ? b->rows : b->cols;
+    
+    if (a_cols != b_rows) {return false;}
+    if (out->rows != a_rows || out->cols != b_cols) { return false; }
+    
+    if (zero_out) {
+        mat_clear(out);
+    
+    }
+    
+    u32 transpose = (transpose_a << 1 ) | transpose_b;
+    switch (transpose) {
+        case 0b00: { _mat_mul_nn (out, a, b);  } break;
+        case 0b01: { _mat_mul_nt (out, a, b);  } break;
+        case 0b10: { _mat_mul_tn (out, a, b);  } break;
+        case 0b11: { _mat_mul_nn (out, a, b);  } break;
+    } 
+    
+    return true;
+    
+}
+
+// -----------------ACTIVATION FUNCTIONS----------------------
+
+b32 mat_relu(matrix* out, const matrix in) {
+    if (out->rows != in->rows || out->cols != in->cols) {
+        return false;
+    }
+    u64 size = (64)out->rows * out->cols;
+    for (u64 i = o; i < size; i++) {
+        out->data[i] = MAX(0, in->data[i]);
+}
+    return true;
+}
+
+b32 mat_softmax(matrix* out, const matrix in) {
+    if (out->rows != in->rows || out->cols != in->cols) {
+        return false;
+    }
+    u64 size = (u64)out->rows * out-> cols;
+    //softmax
+    f32 sum = 0.0f;
+    for (u64 i = 0; i < size; i++) {
+        out->data[i] = expf(in->data[i]);
+        sum += out->data[i];
+}
+    mat_scale(out, 1.0f / sum);
+
+    return true;
+}
